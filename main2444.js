a499038e42fee83c548c1672556d70cb7977b219
anishas07 <Studytea95@gmail.com>
2025-01-03 20:26:38 +1030

    Add files via upload

---
+++ main2444.js
@@ -0,0 +1,12 @@
+
+function sum()
+
+{
+    var a= [10, 20, 30, 40] ;
+document.getElementById("p1").innerHTML =a[0] ;
+
+var b = {"name" : "Anisha", "age" : "15", "school" : "NIHS"};
+document.getElementById("p2").innerHTML = b.age ;
+
+document.getElementById("p3").innerHTML = a.pop() ;
+}
